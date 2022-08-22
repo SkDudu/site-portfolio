78f19c76a4c02cf8a4d61cf09af2c7ad7a2af548
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2022-08-22 10:50:56 -0300

    Update cards.tsx

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,5 +1,5 @@
 import react from 'react';
-import { Horse, Heart, Cube } from "phosphor-react";
+import { Horse, Heart, Cube, FigmaLogo, Sidebar } from "phosphor-react";
 
 export default function Cards(){
     return(
@@ -8,7 +8,9 @@ export default function Cards(){
             <span className="block text-xk tracking-tight font-regular text-gray-500 mt-2 md-4">Serviços que ajudam você a alcançar novos resultados no seu negócio</span>
             <div className='grid grid-cols-3 gap-4 mt-8 mx-auto max-w-7xl px-4'>
                 <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
-                    <img className="h-[300px]" src="https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="Sunset in the mountains"/>
+                    <div className="w-16 h-16 flex justify-center p-4 m-5 bg-neutral-100 rounded">
+                        <FigmaLogo size={32} color="#262626"/>
+                    </div>
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">UI/UX Design</div>
                         <p className="text-gray-700 text-base">
@@ -23,7 +25,10 @@ export default function Cards(){
                 </div>
 
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img className="h-[300px]" src="https://images.unsplash.com/photo-1605907126120-f68611516ecc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+                    <div className="w-16 h-16 flex justify-center p-4 m-5 bg-neutral-100 rounded">
+                        <Sidebar size={32} color="#262626"/>
+                    </div>
+                    
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Web Design</div>
                         <p className="text-gray-700 text-base">
@@ -38,7 +43,9 @@ export default function Cards(){
                 </div>
 
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <img className="h-[300px]" src="https://images.unsplash.com/photo-1548094990-c16ca90f1f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
+                    <div className="w-16 h-16 flex justify-center p-4 m-5 bg-neutral-100 rounded">
+                        <Sidebar size={32} color="#262626"/>
+                    </div>
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Identidade visual</div>
                         <p className="text-gray-700 text-base">
